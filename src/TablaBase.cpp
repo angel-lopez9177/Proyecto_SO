d21d4a35506010bfcfa6fa5f4b22cc644e2b4253
Emanuel <angel.lopez9177@alumnos.udg.mx>
2025-09-28 17:26:08 -0600

    Agrega funciones virtuales a TablaBase para que la integracion de otras tablas sea mas mas simple

--- src/TablaBase.cpp
+++ src/TablaBase.cpp
@@ -8,3 +8,23 @@ void TablaBase::limpiar() {
     this->clearContents();
     this->setRowCount(0);
 }
+
+
+void TablaBase::eliminar(int fila){
+    if (fila < 0 || fila >= this->rowCount()) {
+        return; // Índice fuera de rango
+    }
+    this->removeRow(fila);
+}
+
+void TablaBase::popBack() {
+    if (this->rowCount() > 0) {
+        this->removeRow(this->rowCount() - 1);
+    }
+}
+
+void TablaBase::popFront() {
+    if (this->rowCount() > 0) {
+        this->removeRow(0);
+    }
+}
