0dcf920de55876e4ce174780f37f5fa67a702eb7
Emanuel <angel.lopez9177@alumnos.udg.mx>
2025-09-28 16:39:27 -0600

    Agrega la clase tabla terminado para el visualizados de la tabla de procesos terminados.

---
+++ include/TablaTerminados.h
@@ -0,0 +1,14 @@
+#ifndef TABLATERMINADOS_H
+#define TABLATERMINADOS_H
+
+#include "TablaBase.h"
+
+class TablaTerminados : public TablaBase {
+        Q_OBJECT
+        public:
+                explicit TablaTerminados(QWidget *tabla);
+
+                void agregarProceso(const Proceso& proceso, const QString& operacion, const QString& resultado, int numeroLote);
+        };
+
+#endif // TABLATERMINADOS_H
